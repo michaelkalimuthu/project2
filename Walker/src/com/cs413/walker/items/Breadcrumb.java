bf5c964a12c94182400eb0d499f85dfdeaa89019
alwayslz <shell@shell-pc.att.net>
2012-11-17 00:13:43 -0600

    add more details for breadcrumb

--- Walker/src/com/cs413/walker/items/Breadcrumb.java
+++ Walker/src/com/cs413/walker/items/Breadcrumb.java
@@ -1,12 +1,72 @@
-/**
- * 
- */
 package com.cs413.walker.items;
 
-/**Breadcrumb class will be used in future iterations of application for in-game items
+import java.util.ArrayList;
+
+import com.cs413.walker.locations.DefaultLocation;
+import com.cs413.walker.locations.Location;
+
+/**
+ * Breadcrumb class will be used in future iterations of application for in-game
+ * items
+ * 
  * @author rgiambalvo
  * 
  */
 public class Breadcrumb implements Portable {
+	String name;
+	Location currentLocation;
+	ArrayList<Location> locationHolder;
+
+	/**
+	 * Default constructor.
+	 */
+	public Breadcrumb() {
+		name = Integer.toString((int) (Math.random() * 50000));
+		currentLocation = new DefaultLocation();
+
+	}
+
+	/**
+	 * Creates a Breadcrumb
+	 * 
+	 * @param name
+	 */
+	public Breadcrumb(String name, Location location) {
+		this.name = name;
+		currentLocation = location;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	@Override
+	public void useItem(Location location) {
+		currentLocation = location;
+		locationHolder.add(location);
+		drawDots();
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dropItem(Location location) {
+		currentLocation = location;
+
+		// TODO Auto-generated method stub
+
+	}
+
+	public void drawDots() {
+
+	}
 
 }
--- Walker/src/com/cs413/walker/items/Portable.java
+++ Walker/src/com/cs413/walker/items/Portable.java
@@ -1,5 +1,18 @@
 package com.cs413.walker.items;
 
-public interface Portable {
+import com.cs413.walker.locations.Location;
 
+/**
+ * A interface for portable items
+ * 
+ * @author shell
+ */
+public interface Portable {
+	void setName(String name);
+
+	String getName();
+
+	void useItem(Location location);
+
+	void dropItem(Location location);
 }
